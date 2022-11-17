afe9a63ee4b8a52fde5121956de194a36dcaaf58
Rotem Kalisch <rotem-k@localhost.localdomain>
2022-11-17 14:17:57 +0200

    Cleanup zig_zag

--- zig_zag.py
+++ zig_zag.py
@@ -11,16 +11,15 @@ def create_zetas_dict(start, end):
         zetas['zeta({})'.format(i)] = zeta(i)
     return zetas
 
-def create_zig_zag(n):
+def calculate_zig_zag(n):
     retval = 0
     for i in range(n, 1, -1):
         retval += ((-1) ** (n - i)) * zeta(i)
     retval += (-1) ** (n-1)
     return retval
-    return {'zig_zag({})'.format(n): retval}
 
 def create_zig_zag_dict(n):
-    return {'zig_zag({})'.format(n): create_zig_zag(n)}
+    return {'zig_zag({})'.format(n): calculate_zig_zag(n)}
 
 
 Mx = sympy.Matrix([[(x ** k + (x + 1) ** k + y * ((x + 1) ** (k - 1))), ((-x ** (k - 1)) * (x + y))], [(x + 1) ** k, 0]])
